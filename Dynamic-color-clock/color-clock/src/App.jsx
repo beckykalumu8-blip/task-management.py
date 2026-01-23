83b403efae487041f6c1d65c09619e480e010ba1
beckykalumu <beckykalumu8@gmail.com>
2026-01-24 00:58:44 +0300

    color-clock

--- Dynamic-color-clock/color-clock/src/App.jsx
+++ Dynamic-color-clock/color-clock/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect } from `react`;
+import { format } from `date-fns`;
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [currentTime, setCurrentTime] = useState(``);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        const updateTime =() => {
+            constNow = newDate();
+            const formattedTime = format(now, `HH:mm:ss`);
+            setCurrentTime(formattedTime);
+        };
+
+        updateTime();
+        const intervalId = setInterval(updateTime, 1000);
+        return () => clearInterval(intervalId);
+    })
+
+return (
+    <div className="class-container">
+        <h1>Dynamic Color Clock</h1>
+        <p className="clock-display">{currentTime}</p>
+    </div>
+);
 }
 
-export default App
+export default App;
